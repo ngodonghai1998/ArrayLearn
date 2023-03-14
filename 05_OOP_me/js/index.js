109071885e440cef1acfc5167ad257a7194f96b0
ngodonghai1998 <haiscar17@gmail.com>
2023-03-14 15:12:24 +0700

    version 07

---
+++ 05_OOP_me/js/index.js
@@ -0,0 +1,29 @@
+//Lưu trữ thông tin sinh viên (maSV, tenSinhVien, diaChi, soDienThoai, email)
+
+// var maSoSV = 100;
+// var tenSinhVien = 'Nguyễn Văn A';
+// var diaChi = '117 tan cang binh thanh';
+// var soDienThoai = '09090909';
+// var email = '123456@gmail.com';
+
+// var maSoSV1 = 200;
+// var tenSinhVien1 = 'Nguyễn Văn B';
+// var diaChi1 = '117 tan cang binh thanh';
+// var soDienThoai1 = '08080808';
+// var email1 = 'xyz@gmail.com';
+
+
+//Cách khai báo 1 biến đối tượng
+var  tru = {
+    mau: 100,
+    giap: 100,
+    sucDanh: 1000,
+    ten: 'Trụ A'
+};
+
+//var arr = ['a','b'];
+//arr['0'];
+//arr[0];
+//Truy xuất đến biến trong đối tượng (thuộc tính:property)
+console.log(tru['mau']);
+console.log(tru.mau);
