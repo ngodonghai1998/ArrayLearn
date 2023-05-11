f84f2c116df53f0f669810e735218bfb5d9801e3
ngodonghai1998 <haiscar17@gmail.com>
2023-05-11 20:52:25 +0700

    laptop 41

--- 14_ReactCopo_2/src/Templates/HomeTemplate.jsx
+++ 14_ReactCopo_2/src/Templates/HomeTemplate.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 
 //Component này sẽ đại diện cho component được load trên url
-import { Outlet } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 
 
 export default class HomeTemplate extends Component {
     render() {
         return (
             <div>
+                // Khi làm theo cách này, nhược điểm duy nhất khi gặp phải là thẻ a - thẻ liên kết dẫn đến đường link. Do đó, khi bấm tới ví dụ như Home hay cart thì nó phải load lại trang, không khi đó thì nội dung thay đổi chỉ có trong root, nên chỉ cần load lại trong root là đủ, vậy cần làm gì?
+                // Cần dùng một thẻ khác, gọi là thẻ NavLink, NavLinksẽ giúp ta request đến Outlet mà không cần load lại toàn bộ trang.
+                {/* Chuyển thẻ <a href=''></a> thành <NavLink to=''></NavLink> */}
                 <header className='header p-3 bg-dark text-white'>
                     <nav className='d-flex'>
-                        <a href='/' className='text-white mx-2 nav-link'>Home</a>
-                        <a href='/cart' className='text-white mx-2 nav-link'>Cart (Demo props)</a>
+                        <NavLink to='/' className='text-white mx-2 nav-link'>Home</NavLink>
+                        <NavLink to='/cart' className='text-white mx-2 nav-link'>Cart (Demo props)</NavLink>
                     </nav>
                 </header>
 
