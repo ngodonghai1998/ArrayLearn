a9c4f9155fc52cbb599dbc4a24b32202b4314611
ngodonghai1998 <haiscar17@gmail.com>
2023-05-13 00:13:41 +0700

    pc 42

--- 14_ReactCopo_2/src/ReactFormDemo/CreateProduct.jsx
+++ 14_ReactCopo_2/src/ReactFormDemo/CreateProduct.jsx
@@ -1,11 +1,82 @@
 import React, { Component } from 'react'
 
 export default class CreateProduct extends Component {
-    
+
+    state = {
+        values: {
+            idProduct: '',
+            name: '',
+            price: '',
+            img: '',
+            type: 'phone',
+            desc: '',
+        },
+        errors: {
+            idProduct: '',
+            name: '',
+            price: '',
+            img: '',
+            desc: '',
+        },
+
+    }
+
     handleSubmit = (e) => {
         e.preventDefault();
+
+        //Call api ...
+        let {addProduct} = this.props;
+        //Validation trước thêm vào
+
+        addProduct(this.state.values);
     };
-    
+
+    handleChangeInput = (e) => {
+        let { value, id } = e.target; //e.target chính là thẻ input đang diễn ra sự kiện onInput
+        let dataType = e.target.getAttribute('data-type');
+        let minlength = e.target.getAttribute('data-minlength');
+        let maxlength = e.target.getAttribute('data-maxlength');
+
+        //Xử lý this.state.values
+        let newValue = {...this.state.values};
+        newValue[id] = value;
+
+        //Xử lý errors
+        let newError = {...this.state.errors};
+        let messError = '';
+        if (value.trim() === '') {
+            messError = id + ' không được bỏ trống!'
+        }else {
+            if (dataType) {
+                switch(dataType) {
+                    case 'number': {
+                        let regexNumber = /^\d+$/;
+                        if (!regexNumber.test(value)) {
+                            messError = id + ' phải là số !';
+                        };
+                        break;
+                    }
+                    case 'string': {
+                        let regexNumber = /^[a-zA-Z]+$/;
+                        if (!regexNumber.test(value)) {
+                            messError = id + ' phải là ký tự !';
+                        };
+                        break;
+                    }
+                }
+            }
+        }
+        newError[id] = messError;
+
+        //Cập nhật lại state của cả values và errors sau khi xử lý.
+        this.setState({
+            values: newValue,
+            errors: newError,
+        }, () => {
+            console.log(this.state);
+        })
+    }
+
     render() {
         return (
             <form className='card' onSubmit={this.handleSubmit}>
@@ -15,29 +86,33 @@ export default class CreateProduct extends Component {
                         <div className='col-6'>
                             <div className='form-group'>
                                 <p>Id</p>
-                                <input className='form-control' id='idProduct' name='idProduct' />
+                                <input data-type='number' className='form-control' id='idProduct' name='idProduct' onInput={this.handleChangeInput} />
+                                <p className='text text-danger' >{this.state.errors.idProduct}</p>
                             </div>
 
                             <div className='form-group'>
                                 <p>name</p>
-                                <input className='form-control' id='name' name='name' />
+                                <input data-type='string' className='form-control' id='name' name='name' onInput={this.handleChangeInput} />
+                                <p className='text text-danger' >{this.state.errors.name}</p>
                             </div>
 
                             <div className='form-group'>
                                 <p>price</p>
-                                <input className='form-control' id='price' name='price' />
+                                <input data-type='number' className='form-control' id='price' name='price' onInput={this.handleChangeInput} />
+                                <p className='text text-danger' >{this.state.errors.price}</p>
                             </div>
 
                         </div>
                         <div className='col-6'>
                             <div className='form-group'>
                                 <p>img</p>
-                                <input className='form-control' id='img' name='img' />
+                                <input className='form-control' id='img' name='img' onInput={this.handleChangeInput} />
+                                <p className='text text-danger' >{this.state.errors.img}</p>
                             </div>
 
                             <div className='form-group'>
                                 <p>type</p>
-                                <select className='form-control'>
+                                <select className='form-control' id='type' name='type' onInput={this.handleChangeInput}>
                                     <option value="phone">Phone</option>
                                     <option value="tablet">Tablet</option>
                                     <option value="laptop">Laptop</option>
@@ -46,7 +121,8 @@ export default class CreateProduct extends Component {
 
                             <div className='form-group'>
                                 <p>desc</p>
-                                <input className='form-control' id='desc' name='desc' />
+                                <input data-minlength='6' data-maxlength='32' className='form-control' id='desc' name='desc' onInput={this.handleChangeInput} />
+                                <p className='text text-danger' >{this.state.errors.desc}</p>
                             </div>
                         </div>
                     </div>
--- 14_ReactCopo_2/src/ReactFormDemo/ReactFormDemo.jsx
+++ 14_ReactCopo_2/src/ReactFormDemo/ReactFormDemo.jsx
@@ -2,10 +2,28 @@ import React, { Component } from 'react'
 import CreateProduct from './CreateProduct'
 
 export default class ReactFormDemo extends Component {
+
+    state = {
+        arrProduct: [
+            {idProduct: '01', name: 'IphoneX', price: 1000, type: 'Phone', img:'https://i.pravatar.cc?u=1', desc: 'Điện thoại iphonex'},
+        ],
+    }
+
+    addProduct = (prodInfo) => {
+        console.log(prodInfo);
+
+        this.state.arrProduct.push(prodInfo);
+
+        this.setState({
+            arrProduct: this.state.arrProduct,
+        })
+
+    }
+
     render() {
         return (
             <div className='container'>
-                <CreateProduct />
+                <CreateProduct  addProduct={this.addProduct}/>
 
                 <table className='table mt-2'>
                     <thead className='bg-dark text-white fw-bold'>
@@ -19,6 +37,20 @@ export default class ReactFormDemo extends Component {
                             <th></th>
                         </tr>
                     </thead>
+                    <tbody>
+                        {this.state.arrProduct.map((prod)=> {
+                            return <tr key={prod.idProduct}>
+                                <td>{prod.idProduct}</td>
+                                <td>{prod.name}</td>
+                                <td><img src={prod.img} width={50} height={50} alt={prod.name}/></td>
+                                <td>{prod.price}</td>
+                                <td>{prod.type}</td>
+                                <td>{prod.desc}</td>
+                                <td></td>
+
+                            </tr>
+                        })}
+                    </tbody>
                 </table>
             </div>
         )
